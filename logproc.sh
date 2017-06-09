69c14ba8671f8cdd4362a8912d4b518dd13b474e
Fredrik Dernebo <fredrik@dernebo.se>
2017-06-09 08:52:36 +0200

    Added header info
    Cleaning upp some comments

--- logproc.sh
+++ logproc.sh
@@ -1,8 +1,23 @@
 #!/bin/bash
+# ©Fredrik Dernebo (fredrik@dernebo.se)
+# June 2017
+#
+# Summary: Log memory and cpu usage of a java-process and log to a file.
+# CPU-usage over a certain value logs to a separate warning-file
+#
+# Usage: ./logproc.sh <processname> <update-time> <cpu-warning-threshold>
+#
+# Processname can be checked via command 'jps'
+# Update-time is how often to write to logfiles.
+# CPU-warning-threshold sets when to log to a warning-logfile
+#
+##########################################################################
+# Where to put logfiles
 logpath='/Users/laidback/Library/Logs/Logproc/'
+
+
+##########################################################################
 procname=$1
-#pid=`ps -A |grep -m1 $procname |awk '{print $1}'`
-#pid=$1
 pid=`jps|grep $procname|cut -d" " -f1`
 upd=$2
 thr=$3
@@ -16,9 +31,8 @@ fi
 if [[ $upd == "" ]] ; then upd=1; fi
 if [[ $thr == "" ]] ; then thr=1; fi
 
-echo 'Usage: ./logproc.sh <pid> <update-time> <warning-thr>'
+echo 'Usage: ./logproc.sh <processname> <update-time> <warning-thr>'
 echo 'Update interval: '$upd' seconds'
-#echo 'Warning threshold: '$thr'%'
 echo 'Logging PID '$pid' cpu usage to '$logpath'.'
 while :
         do
@@ -29,7 +43,6 @@ while :
                 curmem=`top -l 2 -pid $pid|tail -1|awk '{ print $8 }'`
                 echo $curtime ' - ' $pid 'cpu: '$curuse'% mem: '$curmem>> $logpath$curlog
                 warncuruse=$(echo "scale=2; $curuse" | bc -l)
-#               echo $warncuruse
                 if (( `echo $warncuruse'>='$thr | bc` ))
                         then echo $curtime ' - ' $pid 'cpu: '$curuse'% mem: '$curmem >> $logpath$curwarnlog
                 fi
